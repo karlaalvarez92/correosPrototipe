331bce6bfb20e2fc9f47349f728d13a3987f8041
karlaalvarezlaboratoria <karla.alvarez.castillo@gmail.com>
2020-02-19 00:42:43 -0600

    Se crea base de prueba

---
+++ src/data.js
@@ -0,0 +1,46 @@
+const data = {
+    "postales": [
+    {
+        "img" : "https://i.ibb.co/WV7WF6d/ET0003.jpg",
+        "price": 20,
+
+    },
+    {
+        "img" : "https://i.ibb.co/Pc8sy9M/ET0205.jpg",
+        "price": 20,
+
+    },
+    {
+        "img" : "https://i.ibb.co/PWGyL70/ET9702.jpg",
+        "price": 20,
+
+    },
+    {
+        "img" : "https://i.ibb.co/nMr5mFb/ET9705.jpg",
+        "price": 20,
+
+    },
+    {
+        "img" : "https://i.ibb.co/zb1NcsB/ET9710.jpg",
+        "price": 20,
+
+    },
+    {
+        "img" : "https://i.ibb.co/Dphb2wc/ET9903.jpg",
+        "price": 20,
+
+    },
+    {
+        "img" : "https://i.ibb.co/rGM5c86/ET9904.jpg",
+        "price": 20,
+
+    },
+    {
+        "img" : "https://i.ibb.co/WVpKd0J/unnamed.jpg",
+        "price": 20,
+
+    },
+
+    ]}
+
+    export default data;
